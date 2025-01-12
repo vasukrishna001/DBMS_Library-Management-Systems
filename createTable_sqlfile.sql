0010e480fa1129883ea04886921d91677006e8d8
vasukrishna001 <159833338+vasukrishna001@users.noreply.github.com>
2025-01-11 20:18:18 -0500

    SqlQueries+CreateTable.sql

--- createTable_sqlfile.sql
+++ createTable_sqlfile.sql
@@ -86,5 +86,99 @@ SELECT * FROM Subscriptions;
 
 
 
+-- Queries used in this project
+
+--Finds the genre which was burrowed by nost of users
+select TT.genre from
+(select genre, count (distinct (transactions.user_id)) as cnt
+from transactions inner join books on transactions.book_id = books.book_id
+group by genre) TT
+where TT.cnt =
+(select max(T.cnt) from
+(select genre, count (distinct (transactions.user_id)) as cnt
+from transactions inner join books on transactions.book_id = books.book_id
+group by genre) T);
+
+
+
+-- Find the age,gender,occupation of users who takes more than 5 days to return the book and the number of times they did 
+-- this was greater than 2 times
+select users.age,users.gender,users.occupation from transactions,users 
+where users.user_id = transactions.user_id and (return_date ::date - borrowed_date::date)>5
+group by users.user_id having count(users.user_id)>2;
+
+
+--- Inserting Author details----
+INSERT INTO Author values ((select max(author_id ) from author) + 1,  'Stephen','ABC')
+
+
+---- Delete transactions that are too old----
+DELETE FROM transactions WHERE ((SELECT CURRENT_DATE) - return_date) > 10;
+
+
+----update country of author using name----
+update author
+set author_country = 'CHINA'
+where 
+author_name = 'stephen'
+
+
+---Inserting Publisher details----
+INSERT INTO publishers values ((select max(publisher_id ) from publishers) + 1,  'SD',8,5.5,6)
+
+
+----update ratings of publisher using id----
+update publishers
+set ratings = 4
+where 
+publisher_id = 3
+
+
+
+---Delete books with less ratings and also remove transcations invovled with those books---
+DELETE FROM transactions WHERE 
+exists (
+select book_id from books where ratings < 1 and books.book_id = transactions.book_id
+);
+DELETE FROM books where ratings<1
+
+
+-- Finds the users who didn't return the book
+select users.user_name
+from transactions,users
+where users.user_id = transactions.user_id and transactions.return_date is null
+group by
+transactions.user_id,users.user_name
+
+--For each genre find the average rating
+select books.genre, AVG(books.ratings)
+from
+books
+group by books.genre
+
+
+--For each price find the average rating
+select books.book_price ,AVG(books.ratings)
+from 
+books
+group by books.book_price
+
+
+-- This query determines how frequently each author's books are borrowed throughout three dynamic time periods--
+WITH CTE_date AS     (
+    SELECT MAX(Borrowed_date) AS last_borrowed FROM Transactions)
+SELECT Aut.Author_name,     
+COUNT(CASE  WHEN Ts.Borrowed_date>=(SELECT  DATE(last_borrowed)-INTERVAL'10 years'  FROM CTE_date) 
+	        THEN 1 END) AS last_10Y ,
+COUNT(CASE WHEN Ts.Borrowed_date>=(SELECT  DATE(last_borrowed) -INTERVAL'3 years'  FROM CTE_date) 
+	  THEN 1 END) AS  last_3Y ,
+COUNT(CASE  WHEN Ts.Borrowed_date>=(SELECT  DATE(last_borrowed)- INTERVAL'1 year'  FROM CTE_date) 
+	  THEN 1 END) AS  last_Y
+FROM Transactions Ts
+JOIN Books  ON Ts.Book_id = Books.Book_id JOIN Author Aut ON Books.Book_authorid = Aut.Author_id
+GROUP BY Aut.Author_name ORDER BY last_10Y DESC, last_3Y DESC, last_Y DESC;
+
+
+
 
 	
